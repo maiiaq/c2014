7be5335820feeefbe5eeaf1f9e5049656bae169c
Alexander Kozlovskiy <alexander.k@ualeaders.com>
2024-01-12 17:39:36 +0100

    initial commit

--- less2/main.py
+++ less2/main.py
@@ -9,7 +9,6 @@ class Human:
        self.job=job
        self.car=car
        self.pet=pet
-
     def get_pet(self):
         if self.money>=10:
             self.money-=5
@@ -18,7 +17,6 @@ class Human:
         else:
             self.gladness-=10
             pass
-
     def get_home(self):
         self.home=House()
     def get_job(self):
@@ -27,6 +25,7 @@ class Human:
         else:
             self.to_repair()
             return
+        self.job=Job(job_list)
     def get_car(self):
         self.car=Auto(brand_of_car)
     def get_eat(self):
@@ -53,32 +52,33 @@ class Human:
         self.satiety-=4
     def get_shopping(self,manage):
         if self.car.drive():
-
+            pass
+        else:
             if self.car.fuel<20:
                 self.get_shopping("fuel")
                 return
-            if manage=="fuel":
-                print("I bought fuel")
-                self.money-=100
-                self.car.fuel+=100
-            elif manage=="food":
-                print("i bought food")
-                self.money-=50
-                self.home.food+=50
-            elif manage=="delicacias":
-                print("i bought delicacias")
-                self.gladness+=10
-                self.satiety+=2
-                self.money-=50
+            else:
+                self.to_repair()
+                return
+        if manage=="fuel":
+            print("I bought fuel")
+            self.money-=100
+            self.car.fuel+=100
+        elif manage=="food":
+            print("i bought food")
+            self.money-=50
+            self.home.food+=50
+        elif manage=="delicacias":
+            print("i bought delicacias")
+            self.gladness+=10
+            self.satiety+=2
+            self.money-=50
     def get_chill(self):
         self.gladness+=10
         self.home.mess+=5
-
     def clean_home(self):
         self.gladness -= 5
         self.home.mess = 0
-
-
     def to_repair(self):
         self.car.strength+=100
         self.money-=50
@@ -95,7 +95,7 @@ class Human:
         home_indexes="Home indexes"
         print(f"{home_indexes:=^50}", "\n")
         print(f"Food-{self.home.food}")
-        print(f"Mess-{self.mess}")
+        print(f"Mess-{self.home.mess}")
         car_indexes=f"{self.car.brand} car.indexes"
         print(f"{car_indexes:=^50}", "\n")
         print(f"Fuel-{self.car.fuel}")
@@ -127,17 +127,17 @@ class Human:
             self.get_job()
             print(f"i don`t have job, i am going to get it{self.job.job} with salary{self.job.salary}")
             self.day_indexes(day)
-            dice=random.randint(1,4)
+        dice=random.randint(1,4)
         if self.satiety<20:
             print("i will go to eat")
-            self.eat()
+            self.get_eat()
         elif self.gladness<20:
             if self.home.mess<15:
                 print("I want to chill, but there is so much mess…\n So I will clean the house")
                 self.clean_home()
             else:
                 print(" let`s chill")
-                self.chill()
+                self.get_chill()
         elif self.money<0:
             print("start work")
             self.get_work()
@@ -146,16 +146,16 @@ class Human:
             self.to_repair()
         elif dice==1:
             print("let`s chill")
-            self.chill()
+            self.get_chill()
         elif dice==2:
             print("start working")
-            self.work()
+            self.get_work()
         elif dice==3:
             print("let`s cleaning")
-            self.work()
+            self.get_work()
         elif dice == 4:
             print("time for treats")
-            self.shopping(manage="delicacias")
+            self.get_shopping(manage="delicacias")
 
 class Auto:
     def __init__(self, brand_list):
@@ -180,7 +180,7 @@ class Pet:
 class House:
     def __init__(self):
         self.food=0
-        self.mess1=0
+        self.mess=0
 brand_of_car={
     "BMW":{"fuel":100, "strength":100, "consumption":12},
     "toyota":{"fuel":50, "strength":60, "consumption":10},
@@ -206,7 +206,7 @@ class Job:
         self.job = random.choice(list(job_list))
         self.salary = job_list[self.job]["salary"]
         self.gladness_less = job_list[self.job]["gladness_less"]
-nick=Human("name=nick")
+nick=Human(name="nick")
 for day in range(1,8):
-    if nick.live==False:
+    if nick.live(day)==False:
         break
